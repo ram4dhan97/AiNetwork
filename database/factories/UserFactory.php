aa98cfe675820faba3b233ad65c347ba80669ec8
unknown <ram4dhan>
2021-08-04 20:12:52 +0700

    update factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,22 +28,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'phone' => $this->faker->unique()->phoneNumber,
             'address' => $this->faker->address,
-            'email_verified_at' => now(),
             'password' => 'password',
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
